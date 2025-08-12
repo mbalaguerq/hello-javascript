6d00425908813a35812894760f8f3cb73d79bffa
mbalaguerq <marc.balaguer.quintana@alumnes.ilernabcn.com>
2025-08-12 19:52:06 +0200

    Fet fins a lliçó 1

--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -2,8 +2,9 @@
 Clase 16 - Variables
 Vídeo: https://youtu.be/1glVfFxj8a4?t=3049
 */
+//3 formes de crear variables: 
 
-// var
+// var. Ja no s'utilitza, pq s'hi pot accedir abans de declarar-la!!
 
 var helloWorld = "¡Hola, JavaScript!"
 console.log(helloWorld)
@@ -19,7 +20,7 @@ console.log(helloWorld2)
 helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
 console.log(helloWorld2)
 
-// const
+// const. Es fa servir per a declarar variables que no es poden modificar després de la seva inicialització.
 
 const helloWorld3 = "¡Hola, JavaScript 3!"
 console.log(helloWorld3)
